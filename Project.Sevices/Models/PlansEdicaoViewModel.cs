ff1505a14040c95f59c634761ad412b50c7d9164
Diego Pereira Salles <diego.salles@wooza.com.br>
2019-09-24 14:31:51 -0300

    ImplementandoViewModel

--- Project.Sevices/Models/PlansEdicaoViewModel.cs
+++ Project.Sevices/Models/PlansEdicaoViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,19 @@ namespace Project.Sevices.Models
 {
     public class PlansEdicaoViewModel
     {
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string SKU { get; set; }
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public int Minutes { get; set; }
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string InternetFranchise { get; set; }
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public decimal PriceOfPlan { get; set; }
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string TypeOfPlan { get; set; }
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string MobileOperator { get; set; }
     }
 }
