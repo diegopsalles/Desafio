5ef0cf76b7a9bca842386752dcc5e354fba0d65b
diego_salles <diego.psalles@gmail.com>
2019-09-23 23:37:48 -0300

    CamadaBLL

--- Project.BLL/Business/PlanBusiness.cs
+++ Project.BLL/Business/PlanBusiness.cs
@@ -1,10 +1,53 @@
-﻿using System;
+﻿using Project.BLL.Contracts;
+using Project.Entities;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using Project.DAL.Contracts;
 
 namespace Project.BLL.Business
 {
-    class PlanBusiness
+    public class PlanBusiness : IPlanBusiness
     {
+        private readonly IPlanRepository _repository;
+
+        public PlanBusiness(IPlanRepository repository)
+        {
+            repository = _repository;
+        }
+
+        public void Atualizar(Plan plan)
+        {
+            _repository.Update(plan);
+        }
+
+        public void Cadastrar(Plan plan)
+        {
+            _repository.Insert(plan);
+        }
+
+        public Plan ConsultarPorID(int IdPlan)
+        {
+            return _repository.GetById(int idPlan);
+        }
+
+        public Plan ConsultarPorMobileOperator(string mobileOperator)
+        {
+            return _repository.GetByMobileOperator(mobileOperator);
+        }
+
+        public Plan ConsultarPorSKU(int sku)
+        {
+            return _repository.GetBySku(sku);
+        }
+
+        public List<Plan> ConsultarTodos()
+        {
+            return _repository.ListAll();
+        }
+
+        public void Excluir(int idPlan)
+        {
+            _repository.Delete(int idPlan);
+        }
     }
 }
--- Project.BLL/Business/RegionBusiness.cs
+++ Project.BLL/Business/RegionBusiness.cs
@@ -1,10 +1,47 @@
-﻿using System;
+﻿using Project.BLL.Contracts;
+using Project.Entities;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using Project.DAL.Contracts;
 
 namespace Project.BLL.Business
 {
-    class RegionBusiness
+    public class RegionBusiness : IRegionBusiness
     {
+        private readonly IRegionRepository _repository;
+
+        public RegionBusiness (IRegionRepository repository)
+        {
+            repository = _repository
+        }
+        public void Atualizar(Region region)
+        {
+            _repository.Atualizar(region);
+        }
+
+        public void Cadastrar(Region region)
+        {
+            _repository.Cadastrar(region);   
+        }
+
+        public Region ConsultarPorDDD(int DDD)
+        {
+            return _repository.GetByDDD(DDD);
+        }
+
+        public Region ConsultarPorID(int IdRegion)
+        {
+            return _repository.GetById(IdRegion);
+        }
+
+        public List<Region> ConsultarTodos()
+        {
+            return _repository.ListAll();
+        }
+
+        public void Excluir(int IdRegion)
+        {
+            _repository.Delete(IdRegion);
+        }
     }
 }
